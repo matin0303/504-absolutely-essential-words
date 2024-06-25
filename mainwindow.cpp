d6825b2bf46284301f3433166b60372810855fb2
matin0303 <pirmohammadimatin@gmail.com>
2024-06-25 15:38:49 +0330

    connect pages

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "secondpage.h"
+
+
 using namespace std;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
